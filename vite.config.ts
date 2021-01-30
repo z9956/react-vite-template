368307ef073673dab2d05adc84b24ca8e56cef1a
9956 <zhz995617@gmail.com>
2021-01-31 01:58:20 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
+// import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   plugins: [reactRefresh()]
 })
