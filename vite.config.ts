fd4551ef147c2044498c2c32724256b045e1cda5
9956 <zhz995617@gmail.com>
2022-07-21 17:48:21 +0800

    chore: 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-    server: {
-        watch: {
-            ignored: /node_modules/,
-            //https://cn.vitejs.dev/config/#server-watch WSL2需配置
-            usePolling: true,
-        },
-    },
-    plugins: [react()]
+  plugins: [react()]
 })
