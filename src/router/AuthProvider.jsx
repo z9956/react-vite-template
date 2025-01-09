c5f93240e1d504de9107659a07e96e32f1d4922f
z9956 <zhz995617@gmail.com>
2025-01-09 13:23:04 +0800

    perf: 路由鉴权优化

--- src/router/AuthProvider.jsx
+++ src/router/AuthProvider.jsx
@@ -1,16 +1,24 @@
+import { memo, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import useAuthStore from '../store/useAuthStore.js';
 import { AuthProviderContext } from '../context/AuthContext.js';
 
-export default function AuthProvider({ children }) {
+const AuthProvider = memo(({ children }) => {
+	const authStore = useAuthStore();
+	const contextValue = useMemo(() => authStore, [authStore]);
+
 	return (
-		<AuthProviderContext.Provider value={useAuthStore()}>
+		<AuthProviderContext.Provider value={contextValue}>
 			{children}
 		</AuthProviderContext.Provider>
 	);
-}
+});
+
+AuthProvider.displayName = 'AuthProvider';
 
 AuthProvider.propTypes = {
 	children: PropTypes.node.isRequired,
 };
+
+export default AuthProvider;
